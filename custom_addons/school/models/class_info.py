287bbfbb63f20cb732c0d1aff916221718ea7b7d
amoljagadambe <amoljagadambe0076@gmail.com>
2019-07-15 16:00:24 +0530

    School Mangement Added

---
+++ custom_addons/school/models/class_info.py
@@ -0,0 +1,42 @@
+from odoo import fields,models,api
+
+
+class ClassInfo(models.Model):
+    
+    _name='class.class'
+    _rec_name = 'class_display_name'
+    
+    name = fields.Char('Class Name', required=True)
+    teacher_name = fields.Char('Teacher Name')
+    division_name = fields.Char('Division Name')
+    monitor_name = fields.Char('Monitor Name')
+    class_display_name = fields.Char(compute="_class_display_name",store=True)
+    student_ids = fields.One2many('student.student','class_id','Students')
+    class_strength = fields.Integer(compute="_count_strength",string='Class Strength')
+    
+    @api.depends('student_ids')
+    def _count_strength(self):
+        for rec in self:
+            rec.class_strength = len(rec.student_ids.ids)
+    
+    
+    @api.depends('name','division_name')
+    def _class_display_name(self):
+        for rec in self:
+            if rec.name and rec.division_name:
+                display_rec_name = rec.name + ' - Div ' + rec.division_name
+                rec.class_display_name = display_rec_name
+    
+    _sql_constraints = [
+        ('class_display_name_uniq', 'unique(class_display_name)', 'Class Name and Division must be unique !'),
+    ]
+    
+#     @api.multi
+#     def name_get(self):
+#         name_list = []
+#         for rec in self:
+#             display_rec_name = rec.name + ' - Div ' + rec.division_name
+#             name_list.append(((rec.id, display_rec_name)))
+#             print(name_list,'================')
+#         return name_list
+#             
---
+++ custom_addons/school/models/student.py
@@ -0,0 +1,57 @@
+from odoo import fields,models,api,_
+from datetime import date
+from odoo.exceptions import UserError
+
+class StudentInfo(models.Model):
+    
+    _name='student.student'
+    _rec_name="name"
+    
+    name = fields.Char('Student Name', required=True)
+    roll_no = fields.Integer('Roll No', required=True)
+    class_name = fields.Char('Class',help="Present class student is in",copy=False)
+    div_name = fields.Char('Division Name')
+    gender = fields.Selection([('male','Male'),('female','Female')], required=True,string="Gender",copy=False)
+    active_student= fields.Boolean('Is Active ?')
+    image = fields.Binary('Student Image')
+    description = fields.Text('Student Description')
+    stay_at = fields.Selection([('parent','Parent'),('guardian','Guardian'),('hostel','Hostel')],default="parent",string="Accomodation At")
+    parent_id = fields.Many2one('res.partner','Parent')
+    parent_name = fields.Char(related="parent_id.name",string='Parent Name')
+    parent_ph_no = fields.Char(related="parent_id.mobile",string='Parent Mobile No')
+    parent_address = fields.Char(related="parent_id.street",string='Parent Address')
+    guardian_name = fields.Char('Guardian Name')
+    guardian_ph_no = fields.Char('Guardian Mobile No')
+    guardian_address = fields.Char('Guardian Address')
+    hostel_name = fields.Char('Hostel Name')
+    hostel_ph_no = fields.Char('Hostel Contact No')
+    hostel_address = fields.Char('Hostel Address')
+    birth_date = fields.Date('Birth Date')
+    age = fields.Integer('Age')
+    class_id = fields.Many2one('class.class','Class')
+    
+    @api.onchange('birth_date')
+    def onchange_date(self):
+        if self.birth_date:
+            today = date.today()
+            age = today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+            if age < 3:
+                raise UserError(_('Student Age should be more than 3 years.'))
+            self.age = age
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
