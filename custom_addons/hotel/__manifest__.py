563399736147585d2d309f86712ef95ee3b9d5fa
amol <amoljagadambe0076@gmail.com>
2019-07-15 22:35:19 +0530

    new model added

---
+++ custom_addons/hotel/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hotel Management',
+    'version': '1.1',
+    'author':'Whiz IT solutions',
+    'category': 'Hotel Management',
+    'description': """
+        Module for Hotel management.
+            """,
+    'depends': ['base'],
+    'data': [],
+    'installable':True,
+}
